ef126cbbe433eb1679e7720bbfe57aff965a089e
J-Stiehl <salsajackie@yahoo.com>
2024-05-18 18:42:30 -0500

    update to pic names in Favorites

--- src/components/pages/Favorites.jsx
+++ src/components/pages/Favorites.jsx
@@ -9,7 +9,19 @@ import {
     Stack,
     Text,
   } from "@chakra-ui/react";
-  import picGr2 from "../../assets/images/favpics/Danika-first-day-of-2nd-grade.jpg"
+ import picK from "../../assets/images/favpics/Danika-first-day-of-Kindergarten.jpg"
+ import picGr1 from "../../assets/images/favpics/Danika-first-day-of 1st-grade.jpg"
+ import picGr2 from "../../assets/images/favpics/Danika-first-day-of-2nd-grade.jpg"
+ import picGr3 from "../../assets/images/favpics/Danika-first-day-of-3rd-grade.jpg"
+ import picGr4 from "../../assets/images/favpics/Danika-first-day-of-4th-grade.jpg"
+ import picGr5 from "../../assets/images/favpics/Danika-first-day-of-5th-grade.jpg"
+ import picGr6 from "../../assets/images/favpics/Danika-first-day-of-6th-grade-cropped.jpg"
+ import picGr7 from "../../assets/images/favpics/Danika-first-day-of-7th-grade.jpg"
+ import picGr8 from "../../assets/images/favpics/Danika-first-day-of-8th-grade.jpg"
+ import picGr9 from "../../assets/images/favpics/Danika-first-day-of-9th-grade.jpg"
+ import picGr10 from "../../assets/images/favpics/Danika-first-day-of-10th-grade.jpg"
+ import picGr11 from "../../assets/images/favpics/Danika-first-day-of-11th-grade.png"
+ import picGr12 from "../../assets/images/favpics/Danika-first-day-of-12th-grade.png"
 
 export default function SchoolPics() {
 
@@ -21,7 +33,8 @@ export default function SchoolPics() {
 <Card maxW='md'>
   <CardBody>
     <Image
-    src="../../assets/images/favpics/Danika-first-day-of-Kindergarten.jpg"
+    // src="../../assets/images/favpics/Danika-first-day-of-Kindergarten.jpg"
+    src={picK}
       alt='Danika in kindergarten'
       borderRadius='lg'
       minHeight="610px"
@@ -41,7 +54,8 @@ export default function SchoolPics() {
 <Card maxW='md'>
   <CardBody>
     <Image
-    src="../../src/assets/images/favpics/Danika-first-day-of 1st-grade.jpg"
+    // src="../../src/assets/images/favpics/Danika-first-day-of 1st-grade.jpg"
+    src={picGr1}
       alt='Danika in 1st grade'
       borderRadius='lg'
     />
@@ -254,7 +268,7 @@ export default function SchoolPics() {
   <CardBody>
     <Image
     src="../src/assets/images/favpics/Danika-first-day-of-12th-grade.png"
-      alt='Danika in 11th grade'
+      alt='Danika in 12th grade'
       borderRadius='lg'
     />
     <Stack mt='6' spacing='3'>
